90a775e9972a7aa28ba147574d94ea90743e1d3d
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-05-12 15:44:34 +0200

    feat: add create and update article admin page

--- src/Controller/Backend/ArticleController.php
+++ src/Controller/Backend/ArticleController.php
@@ -8,6 +8,7 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/article', name: 'admin.article')]
@@ -30,7 +31,7 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/create', name: ".create", methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
+    public function create(Request $request): Response|RedirectResponse
     {
         // Création d'un nouvel objet Article
         $article = new Article();
@@ -48,6 +49,31 @@ class ArticleController extends AbstractController
 
         return $this->render('Backend/Article/create.html.twig', [
             'form' => $form
+        ]);
+    }
+
+    #[Route('/update/{id}', name: '.update', methods: ['GET', 'POST'])]
+    public function update(?Article $article, Request $request): Response|RedirectResponse
+    {
+        if (!$article instanceof Article) {
+            $this->addFlash('error', 'Article not found');
+
+            return $this->redirectToRoute('admin.article.index');
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repo->save($article, true);
+
+            $this->addFlash('success', 'Article modified  successfully');
+
+            return $this->redirectToRoute('admin.article.index');
+        }
+
+        return $this->render('Backend/Article/update.html.twig', [
+            'form' => $form,
         ]);
     }
 }
